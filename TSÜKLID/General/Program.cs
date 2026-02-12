781d5042a89968d59f7e00f6203dc4f5102c6402
Estlib <75094683+Estlib@users.noreply.github.com>
2026-02-12 13:55:33 +0200

    lahendasin ülesande tsüklid-general-1 tervitus koos vooskeemiga

---
+++ TSÜKLID/General/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace General
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // #n1. "Tervitus"
+            // kirjuta programm mis,
+            // - küsib tsükliga kasutajalt tema eesnime
+            string eesNimi = string.Empty;
+            while (eesNimi == string.Empty)
+            {
+                Console.WriteLine("Palun sisesta oma nimi siia: ");
+                eesNimi = Console.ReadLine();
+            }
+            // - - küsitakse uuesti tühja sisendi korral
+            // - küsib kasutajalt tema keskmist nime
+            string keskmineNimi = string.Empty;
+            while (keskmineNimi == string.Empty)
+            {
+                Console.WriteLine("Palun sisesta keskmine nimi siia: ");
+                keskmineNimi = Console.ReadLine();
+            }
+            // - - küsitakse uuesti tühja sisendi korral
+            // - küsib kasutajalt tema perekonnanime
+            string perekonnaNimi = "";
+            while (!(perekonnaNimi != string.Empty))
+            {
+                Console.WriteLine("Palun sisesta ka oma perekonnanimi siia: ");
+                perekonnaNimi = Console.ReadLine();
+            }
+            // - - küsitakse uuesti tühja sisendi korral
+            // - tema vanust
+            int minuVanus = 0;
+            while (minuVanus < 1)
+            { 
+                Console.WriteLine("Palun sisesta ka oma vanus siia: ");
+                minuVanus = int.Parse(Console.ReadLine());
+            }
+            // - - küsitakse uuesti tühja sisendi korral
+            // - ning väljastab talle tervituslause, kasutades kõiki muutujaid
+            Console.WriteLine($"Tere päevast {eesNimi} {perekonnaNimi}! Või kutsun sind {keskmineNimi}? Oled tublisti kasvanud, oled juba {minuVanus}-aastane");
+
+
+            // #n2. "Minu lemmiksnäkk"
+            // kirjuta programm mis
+            // küsib kasutajalt mis on ta lemmiksnäkk
+            // programm kontrollib tsükliga kas järjendis on snäkk olemas
+            // kui tsüklis leitakse snäkk, kuva tekst koos kasutajasisendiga, "jaa :D tean seda, {snäkk} on hea"
+            // kui tsükkel lõppeb ilma snäkki leidmata, kuva tekst "ei tunne kahjuks {snäkk}i :C"
+
+            // #n3. "Metsataimede välimääraja"
+            // kirjuta programm mis
+            // küsib kasutajalt kas ta otsib mingit seent või marja (tsüklis)
+            // kui seent, siis programm esitab seeneloendis olevad seened
+            // ja küsib millise seene kohta infot infojärjendist kuvada
+            // tsükkel siis käib ja otsib teisest järjendis seene infot ja kuvab selle
+            // kui marja, siis programm esitab marjaloendis olevad marja
+            // ja küsib millise marja kohta infot infojärjendist kuvada
+            // tsükkel siis käib ja otsib teisest järjendis marja infot ja kuvab selle
+            // siis küsitakse kas kasutaja tahab mõne marja kohta veel infot, ning tsükkel jätkub
+            // ⭐iseseisvalt lisa juurde puude tuvastamine ⭐
+
+            // #n4. "Stonksid"
+            // kirjuta programm mis töötab tsüklis ja omab tehtavat koodi mis:
+            // küsib kasutajalt temapoolse investeeritava summa
+            // küsib kolme firma kohta millesse ta investeerida soovib (Tesla, TransferWise või Macro$lop)
+            // valitud firma kohta otsustab programm kordaja.
+            // kui selleks on Tesla, siis on kordaja fikseeritud -1.15
+            // kui selleks on TransferWise, siis on kordajaks valemi tuleumus kus juhuarvu abil otsustatakse arv vahemikus 1 ja 100, juhuarv jagatakse 1000ga ja sinna liidetakse 1 juurde
+            // kui selleks on Macro$lop, siis kasutatakse sama valemit TransferWise puhul, aga arv ise on alati negatiivne.
+            // programm küsib ka kasutajalt kui pikaks ajaks (mitu päeva) investeering turul olla lasta
+            // tsükkel kirjutab välja iga päeva kohta firmanime, hetkekordaja, kasutajaportfelliväärtuse
+            // kui kasutaja portfell pole jõudnud alla nulli, siis kasutaja saab valida kas investeerida uuesti või mitte
+            // kui aga portfell on nullis, öeldakse kasutajale et on pankrotis
+            // kuvatakse kasutajale tema portfelli lõppväärtus.
+        }
+    }
+}
