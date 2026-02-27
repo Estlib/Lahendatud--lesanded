d5240cf8c26eafbb09011d0974abf7c0bc013990
Estlib <75094683+Estlib@users.noreply.github.com>
2026-02-27 16:40:58 +0200

    lahendasin meetodite ülesanded 1, 2 ja 4

---
+++ MEETODID/2-Sulgudega kalkulaator/Program.cs
@@ -0,0 +1,144 @@
+﻿namespace _2_Sulgudega_kalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kirjuta, koos sisendikontrolliga ja sisendi normaliseerimisega, programm
+            //küsib kasutajalt kolme tehet, esimene ja kolmas peaksid olema sulgude vahel ✅
+            //kasutaja saab määrata igale tehtele märgi ✅
+            //programm kuvab vastuse vastavalt tehete järjekorrale ✅
+
+            Console.WriteLine("Sulgudega kalkulaator:");
+            string[] tehterida = new string[7];
+            Console.WriteLine("(arv tehe arv) tehe (arv tehe arv)");
+            for (int i = 0; i < tehterida.Length; i++)
+            {
+                //string displayLine = "( ";
+                //foreach (var input in tehterida)
+                //{
+                //    if (input.Contains("0123456789"))
+                //    {
+
+                //    }
+                //}
+                string sisestus = "";
+                do
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine($"Palun sisesta {i + 1}. element, milleks on arv:");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Palun sisesta {i + 1}. element, milleks on tehe:");
+                    }
+                    sisestus = Console.ReadLine();
+                    double checkSisestus = 0;
+
+
+                    //if ((Double.TryParse(sisestus, out checkSisestus) == false) || (sisestus != "-" && sisestus != "*" && sisestus != "/" && sisestus != "+"))
+                    //{
+                    //    bool thisResult = Double.TryParse(sisestus, out checkSisestus);
+                    //        Console.WriteLine(thisResult);
+                    //    sisestus = "";
+                    //    break;
+
+                    //}
+                    //else if (!Double.TryParse(sisestus, out checkSisestus) || (sisestus == "+" && sisestus == "-" && sisestus == "*" && sisestus == "/"))
+                    //{
+                    //    Console.WriteLine("ei ole arv, proovi uuesti");
+                    //    sisestus = "";
+                    //}
+                    //else if (Double.TryParse(sisestus, out checkSisestus) || (sisestus != "+" && sisestus != "-" && sisestus != "*" && sisestus != "/"))
+                    //{Mage
+                    //    Console.WriteLine("ei ole tehtemärk, proovi uuesti");
+                    //    sisestus = "";
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine("eet pänt");
+                    //    sisestus = "";
+                    //}
+                    if ((Double.TryParse(sisestus, out checkSisestus) == false))
+                    {
+                        sisestus = "";
+                        //    Console.WriteLine("ei ole arv, proovi uuesti");
+                        if ((sisestus == "+" && sisestus == "-" && sisestus == "*" && sisestus == "/"))
+                        {
+                            break;
+                        }
+                    }
+                    if ((Double.TryParse(sisestus, out checkSisestus) == false))
+                    {
+                        sisestus = "";
+                        //    Console.WriteLine("ei ole arv, proovi uuesti");
+
+                    }
+
+                } while (sisestus == "");
+
+
+                tehterida[i] = sisestus;
+
+            }
+
+            double tehe = ÜksTehe(
+                [
+                ÜksTehe(
+                    [tehterida[0], tehterida[1], tehterida[2]]
+                    ).ToString(),
+
+                    tehterida[3],
+
+                ÜksTehe(
+                    [tehterida[4], tehterida[5], tehterida[6]]
+                    ).ToString()
+                    ]
+                );
+
+            Console.WriteLine("vastus on " + tehe);
+
+        }
+
+        private static double ÜksTehe(string[] tehterida)
+        {
+            switch (tehterida[1])
+            {
+                case "+":
+                    return Liitmine(double.Parse(tehterida[0]), double.Parse(tehterida[2]));
+                    break;
+                case "-":
+                    return Lahutamine(double.Parse(tehterida[0]), double.Parse(tehterida[2]));
+                    break;
+                case "*":
+                    return Korrutamine(double.Parse(tehterida[0]), double.Parse(tehterida[2])); ;
+                    break;
+                case "/":
+                    return Jagamine(double.Parse(tehterida[0]), double.Parse(tehterida[2]));
+                    break;
+                default:
+                    return 0;
+                    break;
+            }
+
+        }
+
+        public static double Liitmine(double arv1, double arv2)
+        {
+            return arv1 + arv2;
+        }
+        public static double Lahutamine(double arv1, double arv2)
+        {
+            return arv1 - arv2;
+        }
+        public static double Korrutamine(double arv1, double arv2)
+        {
+            return arv1 * arv2;
+        }
+        public static double Jagamine(double arv1, double arv2)
+        {
+            return arv1 / arv2;
+        }
+    }
+}
---
+++ MEETODID/4-Mahukalkulaator/Program.cs
@@ -0,0 +1,112 @@
+﻿
+
+using System.Drawing;
+
+namespace _4_Mahukalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kirjuta programm mis
+            //küsib kasutajalt kas ta tahab arvutada oma kasti mahtu, või õlivaadi mahtu
+            //sisendite küsimiseks on oma meetod, mis ei lase programmil edasi liikuda, kuni vastus ei ole tühi, ega üks võimalikest valikutest.
+            //selle tegemiseks annate meetodile parameetrina valikud listis kaasa ja .Contains abil saate kontrollida kas ta on olemas või mitte
+            //kui programm teab kumba ta arvutab toimuvad järgmised tegevused
+            //  kasti puhul, küsitakse kas kast on kuubik või risttahukas.
+            //    - kuubiku puhul küsitakse küljepikkus, antakse parameetrina meetodile KuubiRuumala() kaasa,
+            //      ning kuubiruumala tagastab double tüüpi andmena muutujasse tehte tulemuse. valem otsi internetist.
+            //    - risttahuka puhul lühima ja pikima külje pikkust ning kõrgust. samamoodi arvutab parameetrite abil 
+            //      meetod RisttahukaRuumala() tulemuse double andmena ja tagastab selle muutujasse.
+            //  Tünni puhul, küsitakse kas ta tünn on kaanega või kaaneta.
+            //    - Kui tal on kaas olemas, siis küsi selle paksust, kui ei ole, määra paksuseks ise 0
+            //      Küsi kasutajalt ka tünni põhja läbimõõtu ja kõrgust ning arvuta SilindriRuumala()
+            //      meetodiga kus parameetrid ka kaasas double tulemus mille tagastad muutujasse
+            //      NB! kaane paksuse arvutad kõrgusest maha, sest kaas võtab õlitünni sees mingi ruumala enda poolt ära.
+            //Kuva kasutajale tema ruumala tulemus peaprogrammis, mitte arvutatavates meetodites.
+            decimal result = 0;
+
+            List<string> options = new List<string>() {"kast", "tünn"};
+            Console.WriteLine("Mille mahtu arvutada tahad?: ");
+            string userSelection = GetAnswer(options);
+            if (userSelection == "kast")
+            {
+                List<string> options2 = new List<string>() { "kuubik", "risttahukas" };
+                Console.WriteLine("Kas ta on üks neist?: ");
+                string userSelection2 = GetAnswer(options2);
+                if (userSelection2 == "kuubik")
+                {
+                    Console.WriteLine("palun sisesta külje pikkus");
+                    decimal side = GetAnswer();
+
+                    result = RisttahukaRuumala(side, side, side);
+                }
+                else
+                {
+                    Console.WriteLine("palun sisesta pika külje pikkus");
+                    decimal side = GetAnswer();
+                    Console.WriteLine("palun sisesta lühikese külje pikkus");
+                    decimal side2 = GetAnswer();
+                    Console.WriteLine("palun sisesta kõrgus");
+                    decimal height = GetAnswer();
+                    result = RisttahukaRuumala(side, side2, height);
+                }
+            }
+            else if (userSelection == "tünn")
+            {
+                decimal lidThickness = 0;
+                List<string> options3 = new List<string>() { "jah", "ei" };
+                Console.WriteLine("Kas su tünn on lahtine või mitte?: ");
+                string userSelection3 = GetAnswer(options3);
+                if (userSelection3 == "ei")
+                {
+                    Console.WriteLine("Palun kirjuta ka selle kaane paksus: ");
+                    lidThickness = GetAnswer();
+                }
+                Console.WriteLine("Mis on tünni läbimõõt?");
+                decimal barrelRadius = (GetAnswer()/2);
+                Console.WriteLine("Kui kõrge on tünn?");
+                decimal height = GetAnswer()-lidThickness;
+                result = SilindriRuumala(barrelRadius, height);
+            }
+            Console.WriteLine($"Sinu {userSelection}i maht on {result} cm3");
+        }
+
+        private static decimal SilindriRuumala(decimal barrelRadius, decimal height)
+        {
+            double radius = (double)barrelRadius;
+            return (decimal)(Math.PI * Math.Pow(radius, 2) * (double)height);
+            //sitanikerdis
+        }
+
+        private static decimal RisttahukaRuumala(decimal a, decimal b, decimal c)
+        {
+            return a * b * c;
+        }
+
+        private static decimal GetAnswer()
+        {
+            decimal answer = 0;
+            do
+            {
+                Console.Write("Kirjuta reaalne mõõt ->");
+                answer = decimal.Parse(Console.ReadLine());
+                Console.WriteLine();
+            } while (answer <= 0);
+            return answer;
+        }
+        private static string GetAnswer(List<string> options)
+        {
+            Console.WriteLine("Valikus on:");
+            foreach (string option in options) { Console.WriteLine(option); }
+            string thisOption = "";
+            do
+            {
+                Console.Write("Kirjuta sobiv sisestus ->");
+                thisOption = Console.ReadLine().ToLower();
+                Console.WriteLine();
+            } while (!options.Contains(thisOption));
+            return thisOption;
+        }
+    }
+}
